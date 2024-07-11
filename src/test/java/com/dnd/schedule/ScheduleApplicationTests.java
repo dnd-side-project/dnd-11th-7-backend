03b4ea448140c23d01fe3a71c06c5a8952a1c191
seungjo <f1v3@kakao.com>
2024-07-11 14:46:19 +0900

    chore: initial commit

---
+++ src/test/java/com/dnd/schedule/ScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dnd.schedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
