3e582bc843feb8060c38626f96e38f2c87bff0af
seungjo <f1v3@kakao.com>
2024-07-25 15:20:01 +0900

    feat: 테스트 Application 명 변경

--- src/test/java/com/dnd/jjakkak/JjakkakApplicationTests.java
+++ src/test/java/com/dnd/jjakkak/JjakkakApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ZzaekkacApplicationTests {
+class JjakkakApplicationTests {
 
     @Test
     void contextLoads() {
