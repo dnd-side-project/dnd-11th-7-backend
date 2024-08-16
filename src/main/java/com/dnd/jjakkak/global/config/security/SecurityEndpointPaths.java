250a436271dc2de93b889439b48e7f6403b08a5a
seungjo <f1v3@kakao.com>
2024-08-16 16:46:16 +0900

    refactor: 테스트 로그인 추가 (화이트 리스트)

--- src/main/java/com/dnd/jjakkak/global/config/security/SecurityEndpointPaths.java
+++ src/main/java/com/dnd/jjakkak/global/config/security/SecurityEndpointPaths.java
@@ -10,8 +10,9 @@ public class SecurityEndpointPaths {
 
     public static final String[] WHITE_LIST = {
             "/api/v1/auth/oauth/**",
-            "/api/v1/check-auth",
-            "/api/v1/meeting/**"
+            "/api/v1/auth/**",
+            "/api/v1/meeting/**",
+            "/test/login"
     };
     public static final String[] USER_LIST = {
             "/api/v1/categories",
@@ -24,5 +25,4 @@ public class SecurityEndpointPaths {
     private SecurityEndpointPaths() {
         // 인스턴스 생성 방지
     }
-
 }
