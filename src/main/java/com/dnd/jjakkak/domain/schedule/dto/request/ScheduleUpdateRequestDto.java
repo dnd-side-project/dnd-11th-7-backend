879a95a600ffe01f03dddf608032f7c1363b47c7
seungjo <f1v3@kakao.com>
2024-09-07 22:55:29 +0900

    fix: 일정 수정 요청 DTO - 유효성 검사 수정

--- src/main/java/com/dnd/jjakkak/domain/schedule/dto/request/ScheduleUpdateRequestDto.java
+++ src/main/java/com/dnd/jjakkak/domain/schedule/dto/request/ScheduleUpdateRequestDto.java
@@ -1,14 +1,14 @@
 package com.dnd.jjakkak.domain.schedule.dto.request;
 
 import com.dnd.jjakkak.domain.dateofschedule.dto.request.DateOfScheduleCreateRequestDto;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- * {class name}.
+ * 일정 수정 요청 DTO 클래스입니다.
  *
  * @author 정승조
  * @version 2024. 07. 31.
@@ -16,7 +16,7 @@ import java.util.List;
 @Getter
 public class ScheduleUpdateRequestDto {
 
-    @NotNull(message = "일정 날짜를 입력해주세요.")
+    @Size(min = 1, message = "일정 날짜를 최소 1개 이상 입력해주세요.")
     private final List<DateOfScheduleCreateRequestDto> dateOfScheduleList = new ArrayList<>();
 
     private String scheduleNickname;
