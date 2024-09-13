0e7f5a0a3d6bdf177939b18b7e042bf1aef4ed35
seungjo <f1v3@kakao.com>
2024-09-13 16:39:53 +0900

    refactor: 로그아웃 핸들러 리팩토링

--- src/main/java/com/dnd/jjakkak/domain/jwt/handler/OAuth2LogoutHandler.java
+++ src/main/java/com/dnd/jjakkak/domain/jwt/handler/OAuth2LogoutHandler.java
@@ -1,5 +1,7 @@
 package com.dnd.jjakkak.domain.jwt.handler;
 
+import com.dnd.jjakkak.domain.jwt.provider.JwtProvider;
+import com.dnd.jjakkak.domain.member.repository.MemberRepository;
 import com.dnd.jjakkak.domain.member.service.BlacklistService;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
@@ -11,43 +13,53 @@ import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
+import java.util.Arrays;
 
 /**
  * 로그아웃 시 토큰에 빈 값을 넣어 전송 하는 방식으로 토큰을 삭제하는 핸들러입니다.
  *
- * @author 류태웅
+ * @author 류태웅, 정승조
  * @version 2024. 09. 13.
  */
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class OAuth2LogoutHandler implements LogoutHandler {
+
+    private final MemberRepository memberRepository;
     private final BlacklistService blacklistService;
+    private final JwtProvider jwtProvider;
 
     @Override
     public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
-        String refreshToken = null;
-
-        Cookie[] cookies = request.getCookies();
-        if (cookies != null) {
-            for (Cookie cookie : cookies) {
-                if ("refresh_token".equals(cookie.getName())) {
-                    refreshToken = cookie.getValue();
-                    break;
-                }
-            }
-        }
-
         log.debug("logout 시작");
-        // todo : 새로운 refresh_token 검증 방법 필요
-        if (refreshToken != null) {
-            log.debug("logout refreshToken: {}", refreshToken);
-            LocalDateTime expirationDate = LocalDateTime.now().plusWeeks(1);
-            blacklistService.blacklistToken(refreshToken, expirationDate);
-            log.debug("logout 성공");
-        } else {
+
+        String refreshToken = extractRefreshToken(request.getCookies());
+        Long kakaoId = Long.parseLong(jwtProvider.validateToken(refreshToken));
+
+        if (refreshToken == null || memberRepository.existsByKakaoId(kakaoId)) {
             log.debug("쿠키 인증 오류");
             response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return;
         }
+
+        log.debug("logout refreshToken: {}", refreshToken);
+        LocalDateTime expirationDate = LocalDateTime.now().plusWeeks(1);
+        blacklistService.blacklistToken(refreshToken, expirationDate);
+        log.debug("logout 성공");
+    }
+
+    /**
+     * 쿠키에서 Refresh Token을 추출합니다.
+     *
+     * @param cookies 모든 쿠키
+     * @return Refresh Token
+     */
+    public String extractRefreshToken(Cookie[] cookies) {
+        return Arrays.stream(cookies)
+                .filter(cookie -> "refresh_tokne".equals(cookie.getName()))
+                .findFirst()
+                .map(Cookie::getValue)
+                .orElse(null);
     }
 }
