832c3144c0d39097a7ad0b23cf3321183ad3ac43
seungjo <f1v3@kakao.com>
2024-09-15 23:58:08 +0900

    feat: 권한 체크 API 추가 및 수정

--- src/main/java/com/dnd/jjakkak/global/config/security/SecurityEndpointPaths.java
+++ src/main/java/com/dnd/jjakkak/global/config/security/SecurityEndpointPaths.java
@@ -11,13 +11,17 @@ public class SecurityEndpointPaths {
     public static final String[] WHITE_LIST = {
             "/api/v1/auth/oauth/**",
             "/api/v1/auth/**",
-            "/api/v1/meetings/**",
+            "/api/v1/meetings/*/info",
+            "/api/v1/meetings/*/times",
+            "/api/v1/meetings/*/participants",
             "/api/v1/meetings/*/schedules/guests/**"
     };
 
     public static final String[] USER_LIST = {
             "/api/v1/categories",
-            "/api/v1/member/**"
+            "/api/v1/member/**",
+            "/api/v1/meetings",
+            "/api/v1/meetings/*/schedules/members/**"
     };
 
     public static final String[] ADMIN_LIST = {
